5bc5bf04ae862c4a8a895eca8a77276aa137b41c
Tianlong1089 <109485639+Tianlong1089@users.noreply.github.com>
2023-01-18 15:33:14 -0600

    Add files via upload
    Codigo implementacion Bluetooth Arduino

---
+++ clasebt.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+SoftwareSerialBT(10,11);
+void setup() {
+ BT.begin(9600);
+ Serial.begin(9600);
+ while(!serial);
+ while(!BT);
+ Serial.print("Ambos dispositivos contectados");
+
+}
+
+void loop() {
+  BT.print("AT");
+  BT.print("AT+NAME");
+  BT.print("LOCOTES");
+ 
+  if(BT.available()){
+  Serial.print(BT.read());
+  }
+  while(1);
+  
+}
