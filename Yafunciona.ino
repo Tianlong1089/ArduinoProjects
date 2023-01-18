bbe158447e8a16cfb98273608c4cd4ed2dcf7c66
Tianlong1089 <109485639+Tianlong1089@users.noreply.github.com>
2023-01-18 15:29:15 -0600

    Add files via upload

---
+++ Yafunciona.ino
@@ -0,0 +1,33 @@
+int numero=0;
+void setup() {
+
+  pinMode(13, OUTPUT);
+}
+
+
+void loop() {
+  
+ creciente();
+}
+
+ void creciente(){
+  
+ 
+   for (numero=0;numero<=2048;numero++) {
+ digitalWrite(13, HIGH);   
+  delayMicroseconds((numero));                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delayMicroseconds(2048-(numero)); 
+   
+   }
+  
+    for (numero=0;numero<=2048;numero++){
+   digitalWrite(13, HIGH);
+   delayMicroseconds(2048- (numero));
+   digitalWrite(13, LOW);
+   delayMicroseconds(numero);
+  
+   }
+  
+   
+    }
